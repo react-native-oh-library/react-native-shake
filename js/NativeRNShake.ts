22a9c90573751c7ce0f300001911199b9a2b2786
BigDream <18795973943@163.com>
2024-12-18 10:26:19 +0800

    chore: 五项核查整改 (#4)
    Co-authored-by: wushuwang <wushuwang1@h-partners.com>

--- js/NativeRNShake.ts
+++ js/NativeRNShake.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { TurboModule, TurboModuleRegistry } from 'react-native'
 
 interface Spec extends TurboModule {
